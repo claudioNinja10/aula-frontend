eda096ed087a7f7362e29208afbd9911e7a94e16
CLAUDIO DOMINGOS CAPANELA CALIPI <claudio_calipi@estudante.sc.senai.br>
2024-09-19 22:09:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <header>Cabecalho</header>
+      <aside>Lado Esquerdo</aside>
+      <main>Principal</main>
+      <footer>Rodapé</footer>
+    </>
+
+  );
+}
+
+export default App;
